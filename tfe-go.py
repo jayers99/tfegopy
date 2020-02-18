49fdcef182535e413aed297798ffdc7667edcd48
John Ayers <jayers99@gmail.com>
2020-02-17 19:59:45 -0800

    add env arg

--- tfe-go.py
+++ tfe-go.py
@@ -5,6 +5,7 @@ import re
 from pprint import pprint as pp
 import requests
 import json
+import argparse
 import pyterprise
 
 
@@ -29,6 +30,8 @@ def get_env_config(env):
     try:
         with open('env_configs.json', mode='rt', encoding='UTF-8') as f:
             env_confs = json.load(f)
+        if env == 'default':
+            env = env_confs['default']
         env_conf = env_confs['envs'][env]
         return env_conf
     except:
@@ -37,14 +40,21 @@ def get_env_config(env):
 
 
 def main():
-    env_conf = get_env_config('dev')
+    parser = argparse.ArgumentParser(description='Do a bunch of cool stuff to TFE.')
+    parser.add_argument('--env', metavar='Environment', type=str, default='default',
+                        help='Environment you want to use defined in env_configs.json')
+    args = parser.parse_args()
+    #print(args.env)
+
+    env_conf = get_env_config(args.env)
+    pp(env_conf)
     tfe_host = env_conf['tfe_host']
     tfe_org = env_conf['tfe_org']
     tfe_vcs_oauth_token_id = env_conf['tfe_vcs_oauth_token_id']
-    pp(env_conf)
+
     # your secret tfe api token
     tfe_token = get_token(tfe_host)
-    print(tfe_token)
+    #print(tfe_token)
 
 
 if __name__ == '__main__':
